1172185fb02725fc6f9c4f434a430055b4374873
Tanjona <andrianiainatanjona67@gmail.com>
2025-10-07 14:38:13 +0300

    test

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { useLangStore } from "../store/languageStore.jsx";
-import { useTranslation } from "react-i18next";
+// import React from "react";
+// import { useLangStore } from "../store/languageStore.jsx";
+// import { useTranslation } from "react-i18next";
 
-export default function LanguageSwitcher() {
-  const { lang, setLang } = useLangStore();
-  const { i18n } = useTranslation();
+// export default function LanguageSwitcher() {
+//   const { lang, setLang } = useLangStore();
+//   const { i18n } = useTranslation();
   
-  const toggleLang = (event) => { 
-    const newLang = event.target.value;
-    setLang(newLang);
-    i18n.changeLanguage(newLang);
-  }
-  return (
-    <>
-    <select onChange={toggleLang} className="  border border-gray-300 rounded-md px-2 py-1 text-sm  shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 ">
-      <option className="text-black" value="fr">Français</option>
-      <option className="text-black" value="en">English</option>
-      <option className="text-black" value="mg">Malagasy</option>
-    </select>
-    </>
-  );
+//   const toggleLang = (event) => { 
+//     const newLang = event.target.value;
+//     setLang(newLang);
+//     i18n.changeLanguage(newLang);
+//   }
+//   return (
+//     <>
+//     <select onChange={toggleLang} className="  border border-gray-300 rounded-md px-2 py-1 text-sm  shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 ">
+//       <option className="text-black" value="fr">Français</option>
+//       <option className="text-black" value="en">English</option>
+//       <option className="text-black" value="mg">Malagasy</option>
+//     </select>
+//     </>
+//   );
 
-}
+// }
