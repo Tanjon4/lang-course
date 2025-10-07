3afb358600ac0d708d114f01de287cfcc55b35cb
Tanjona <andrianiainatanjona67@gmail.com>
2025-10-07 15:42:06 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-// "use client";
+"use client";
 
-// import { useTranslation } from "react-i18next";
-// import "@/translations/i18next"; // antsoina ny config
+import { useTranslation } from "react-i18next";
+import "@/translations/i18next"; // antsoina ny config
 import HeroSection from "@/components/sections/HeroSection";
-// import useLanguageStore from "@/store/languageStore";
-// import { useEffect } from "react";
-// import i18n from "@/translations/i18next";
+import useLanguageStore from "@/store/languageStore";
+import { useEffect } from "react";
+import i18n from "@/translations/i18next";
 
 export default function Home() {
-  // const { t } = useTranslation();
-  // const { language } = useLanguageStore();
+  const { t } = useTranslation();
+  const { language } = useLanguageStore();
 
-  // // Raha ovaina ny lang dia miova koa ny i18next
-  // useEffect(() => {
-  //   i18n.changeLanguage(language);
-  // }, [language]);
+  // Raha ovaina ny lang dia miova koa ny i18next
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [language]);
 
   return (
     <main className="p-6">
