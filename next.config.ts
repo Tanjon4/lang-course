de188c7eeca8560a81707afbded38434d4f2d2dc
Mamy <mamitianarandrianantenaina253@gmail.com>
2025-10-21 12:13:05 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,28 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // ✅ Ignore les erreurs ESLint pendant le build
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // ✅ Ignore aussi les erreurs TypeScript (optionnel)
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['localhost', 'your-domain.com'],
+    domains: [
+      'localhost',
+      'e-learning-eta-ten.vercel.app'
+      ],
   },
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
   },
-
-
-  // ✅ Optimisations recommandées
   reactStrictMode: true,
+  // Configuration pour résoudre le problème des lockfiles
+  turbopack: {
+    root: process.cwd(),
+  },
+
 };
 
-
 export default nextConfig;
-
-// next.config.js
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     appDir: true,
-//   },
-//   images: {
-//     domains: ['localhost', 'your-domain.com'],
-//   },
-//   env: {
-//     API_BASE_URL: process.env.API_BASE_URL,
-//   },
-// }
-
-// module.exports = nextConfig
-
-
