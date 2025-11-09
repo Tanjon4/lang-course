bb7c03d54ab06f648fbc835449d6dae56eea9712
Mamy <mamitianarandrianantenaina253@gmail.com>
2025-11-09 17:55:27 +0300

    bug

--- src/app/[lang]/courses/[courseId]/[levelId]/[chapterId]/page.tsx
+++ src/app/[lang]/courses/[courseId]/[levelId]/[chapterId]/page.tsx
@@ -1,19 +1,17 @@
 // app/[lang]/courses/page.tsx
 'use client';
 
-import Layout from "@/components/layout/BaseLayout";
-import { useCourses } from '@/lib/hooks/useCourses';
 
 
 export default function CoursesPage() {
   // return <CoursesComponent />;
   return(
-    <Layout>
+    <>
         <br /> <br />
         <h1 className="text-2xl font-bold">ireto avy ny lesona ao amin'io chapitre io </h1>
         
-       
-    </Layout>
+    </>  
+    
   )
 
 }
